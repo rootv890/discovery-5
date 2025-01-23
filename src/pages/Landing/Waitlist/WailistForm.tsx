8ba93cbce2606223a6a82a03e7102a2b9683a5f1
ROOTV890 <prootv.31@gmail.com>
2025-01-23 20:44:05 +0530

    fix merge conflict

--- src/pages/Landing/Waitlist/WailistForm.tsx
+++ src/pages/Landing/Waitlist/WailistForm.tsx
@@ -1,4 +1,4 @@
-import { set, SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 import { useNavigate } from "react-router";
 import { TbLoader2 } from "react-icons/tb";
